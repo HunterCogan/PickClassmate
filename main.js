3c2c0bc517b5a16e82a28dff433fb7a383ca7d6b
Hunter Cogan <robert.h.cogan@gmail.com>
2021-04-09 15:12:47 -0400

    add ability to change name pool

--- main.js
+++ main.js
@@ -7,17 +7,54 @@ const resHold = $('#result-hold');
 const tempRes = $('#tempRes');
 
 //name arr
-const names = ['Andres', 'Cody', 'Cynthia', 'Daniela', 'David', 'Dicky', 'Francisco', 'Hunter', 'Jesper', 'Joey', 'Jonny', 'Juan', 'Sumeet'];
+let names = ['Andres', 'Cody', 'Cynthia', 'Daniela', 'David', 'Dicky', 'Francisco', 'Hunter', 'Jesper', 'Joey', 'Jonny', 'Juan', 'Sumeet'];
 
-//save repeatition
+//render names in toggle btns
+const getNames = (arr) => {
+    arr.forEach((e, i) => {
+        $('#name-row').innerHTML += `<div id="${e}" class="nameBtn" onclick="clickName('${e}')">${e}</div>`
+    });
+};
+getNames(names);
+
+//handle click names
+const clickName = (n) => {
+    //global this
+    let temp = $(`#${n}`);
+
+    //handle toggle
+    if (temp.classList.contains('canceled')) {
+        //toggle to normal
+        temp.style.backgroundColor = '#fff';
+        temp.classList.toggle('canceled');
+
+        //add name back to arr
+        names.push(`${n}`);
+        console.log(names);
+    } else {
+        //toggle off
+        temp.classList.toggle('canceled');
+        temp.style.backgroundColor = '#ffb3b3';
+
+        //remove name from arr
+        names = names.filter(e => e !== n);
+        console.log(names)
+    }
+};
+
+
+
+//save repetition
 const rm = (type, arr) => type==='color' ? Math.floor(Math.random() * 255) :
-    type==='name' ? Math.floor(Math.random() * (arr.length - 1)) :
+    type==='name' ? Math.floor(Math.random() * arr.length) :
         console.log('rm type error');
 
 const rc = () => `rgb(${rm('color')}, ${rm('color')}, ${rm('color')})`;
 
 const clickWrapper = () => {
     $('#btn-hold button').onclick = function () {
+        console.log(names.length);
+
         //set dynamic speed change variables
         let num = 0;
         let int = 100;
